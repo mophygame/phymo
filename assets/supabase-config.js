e4f886ed4c945f7507e05b0aecec61874ac8a06c
mophygame <mophygame@gmail.com>
2026-05-22 14:04:26 +0800

    Add files via upload

---
+++ assets/supabase-config.js
@@ -0,0 +1,9 @@
+window.PHYMO_SUPABASE = {
+  url: "https://cqdomykjvqwffhyueqft.supabase.co",
+  anonKey: "sb_publishable_tqU5a4MW5VedI7my9SfWuQ_sF_idqSs",
+  profileTable: "profiles",
+  nicknameColumn: "nickname",
+  verifyLoginFunction: "verify_phymo_login",
+  musicListFunction: "get_available_music",
+  loveLetterFunction: "get_my_love_letter"
+};
